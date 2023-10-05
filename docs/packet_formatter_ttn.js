c61a10bcff0cd8bc0b2a52b7f58eb735660c2cc4
nguyenmanhthao996tn <manhthao.spm@gmail.com>
2023-10-05 14:36:46 +0200

    Update: packet_formatter_ttn.js to convert to unsigned numbers

--- docs/packet_formatter_ttn.js
+++ docs/packet_formatter_ttn.js
@@ -2,20 +2,20 @@ function decodeUplink(input) {
   if (input.bytes.length == 27) {
     var packet_type = (input.bytes[0] === 1) ? 'satellite' : 'terrestrial';
 
-    var send_epoch = (input.bytes[4] << 0) | (input.bytes[3] << 8) | (input.bytes[2] << 16) | (input.bytes[1] << 24);
+    var send_epoch = convert_to_unsigned((input.bytes[4] << 0) | (input.bytes[3] << 8) | (input.bytes[2] << 16) | (input.bytes[1] << 24));
 
-    var gnss_latitude = (input.bytes[8] << 0) | (input.bytes[7] << 8) | (input.bytes[6] << 16) | (input.bytes[5] << 24);
+    var gnss_latitude = convert_to_unsigned((input.bytes[8] << 0) | (input.bytes[7] << 8) | (input.bytes[6] << 16) | (input.bytes[5] << 24));
     gnss_latitude /= 1e6;
-    var gnss_longitude = (input.bytes[12] << 0) | (input.bytes[11] << 8) | (input.bytes[10] << 16) | (input.bytes[9] << 24);
+    var gnss_longitude = convert_to_unsigned((input.bytes[12] << 0) | (input.bytes[11] << 8) | (input.bytes[10] << 16) | (input.bytes[9] << 24));
     gnss_longitude /= 1e6;
 
-    var next_pass_epoch = (input.bytes[16] << 0) | (input.bytes[15] << 8) | (input.bytes[14] << 16) | (input.bytes[13] << 24);
+    var next_pass_epoch = convert_to_unsigned((input.bytes[16] << 0) | (input.bytes[15] << 8) | (input.bytes[14] << 16) | (input.bytes[13] << 24));
 
-    var next_pass_duration = (input.bytes[20] << 0) | (input.bytes[19] << 8) | (input.bytes[18] << 16) | (input.bytes[17] << 24);
+    var next_pass_duration = convert_to_unsigned((input.bytes[20] << 0) | (input.bytes[19] << 8) | (input.bytes[18] << 16) | (input.bytes[17] << 24));
 
-    var next_gnss_update_epoch = (input.bytes[24] << 0) | (input.bytes[23] << 8) | (input.bytes[22] << 16) | (input.bytes[21] << 24);
+    var next_gnss_update_epoch = convert_to_unsigned((input.bytes[24] << 0) | (input.bytes[23] << 8) | (input.bytes[22] << 16) | (input.bytes[21] << 24));
 
-    var battery_voltage = (input.bytes[26] << 0) | (input.bytes[25] << 8);
+    var battery_voltage = convert_to_unsigned((input.bytes[26] << 0) | (input.bytes[25] << 8));
     battery_voltage = (battery_voltage / 4095) * 3.3;
     battery_voltage *= 1.5; // This ratio is on-board voltage divider (R2/R4)
 
@@ -44,4 +44,10 @@ function decodeUplink(input) {
       errors: []
     };
   }
+}
+
+function convert_to_unsigned(input_number)
+{
+  var output = input_number >>> 0;
+  return output;
 }
